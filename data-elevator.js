d913875831feb6a905d24c1d212b7d17fc0dd288
wei <124412877+wei7122@users.noreply.github.com>
2024-12-18 12:09:08 +0800

    Add files via upload

---
+++ data-elevator.js
@@ -0,0 +1,33 @@
+/*
+ ** TODO: 電梯DATA ******************
+ */
+// 小網下方固定按鈕圖片位置(left: 左圖右文 top: 上圖下文)
+var iconPosition = "left";
+
+// 小網下方固定按鈕(文案、連結;請不要超過五組)
+var iconName = [
+  {
+    "mobileLink": "https://media.etmall.com.tw/sp/22936/m/index.html",
+    "appLink": "etmall://open?pgid=44&tb=0&url=https%3A%2F%2Fmedia.etmall.com.tw%2Fsp%2F22936%2Fm%2Fapp.html&t=test%20bottom%20nav%7CETMall%E6%9D%B1%E6%A3%AE%E8%B3%BC%E7%89%A9",
+    "imageUrl": "icon-2.png",
+    "words": "B滿額贈",
+  },
+  {
+    "mobileLink": "",
+    "appLink": "",
+    "imageUrl": "icon-1.png",
+    "words": "A高額",
+  },
+  {
+    "mobileLink": "",
+    "appLink": "",
+    "imageUrl": "icon-3.png",
+    "words": "B滿額贈",
+  },
+  {
+    "mobileLink": "",
+    "appLink": "",
+    "imageUrl": "",
+    "words": "B滿額贈",
+  },
+]
